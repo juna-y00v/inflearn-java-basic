977a4f93b24f3c6831937e22d2777ae4b2a3da2f
Yjunha <junaY00v@gmail.com>
2024-06-21 22:20:56 +0900

    Section 3. 객체 지향 프로그래밍 - 문제와 풀이

---
+++ src/oop1/ex/Account.java
@@ -0,0 +1,22 @@
+package oop1.ex;
+
+public class Account {
+    int balance;
+
+
+    void deposit(int amount) {
+        balance += amount;
+    }
+
+    void withdraw(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("잔액 부족");
+        }
+    }
+
+    void showBalance() {
+        System.out.println("잔고: " + balance);
+    }
+}
