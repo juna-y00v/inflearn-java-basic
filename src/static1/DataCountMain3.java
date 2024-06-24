906c21c0f44cfd14aa81eae4642864793bc7f25e
Yjunha <junaY00v@gmail.com>
2024-06-24 17:31:47 +0900

    Section 7. 자바 메모리 구조와 static - static 메서드 1

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -4,14 +4,20 @@ public class DataCountMain3 {
 
     public static void main(String[] args) {
         Data3 data1 = new Data3("A");
-        System.out.println("A count = " +Data3.count);
+        System.out.println("A count = " + Data3.count);
 
         Data3 data2 = new Data3("B");
-        System.out.println("B count = " +Data3.count);
+        System.out.println("B count = " + Data3.count);
 
         Data3 data3 = new Data3("C");
-        System.out.println("C count = " +Data3.count);
+        System.out.println("C count = " + Data3.count);
 
+        //추가
+        //인스턴스를 통한 접근
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count); // 권장하진 않는다. 인스턴스 변수인지 클래스 변수인지 한번에 알기가 어려움
 
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
     }
 }
