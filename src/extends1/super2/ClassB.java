838f47049a33571988dccf7c4da7db63290f6c57
Yjunha <junaY00v@gmail.com>
2024-06-25 18:54:04 +0900

    Section 9. 상속 - super 생성자

--- src/extends1/super2/ClassB.java
+++ src/extends1/super2/ClassB.java
@@ -3,9 +3,8 @@ package extends1.super2;
 public class ClassB extends ClassA{
 
     public ClassB(int a) {
-        this(10,20); // 기본 생성자 생략 가능
-        
-        System.out.println("ClassB 생성자 a = "+ a);
+        this(10, 20); // 기본 생성자 생략 가능
+        System.out.println("ClassB 생성자 a = " + a);
     }
 
     public ClassB(int a, int b) {
