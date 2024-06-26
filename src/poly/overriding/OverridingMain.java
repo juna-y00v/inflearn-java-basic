ba1d4367c44087caa4fe8cb73c3c47e59ed7a898
Yjunha <junaY00v@gmail.com>
2024-06-26 15:36:47 +0900

    Section 11. 다형성2 - 다형성 활용1

--- src/poly/overriding/OverridingMain.java
+++ src/poly/overriding/OverridingMain.java
@@ -20,5 +20,6 @@ public class OverridingMain {
         System.out.println("Parent -> Parent");
         System.out.println("value = " +poly.value); //변수는 오버라이딩X
         poly.method(); //메서드 오버라이딩!
+
     }
 }
